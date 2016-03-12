cfd82469c8e9a065879ae82584b464521c0b0f21
Potoman <lepotoman@msn.com>
2016-03-12 22:48:35 +0100

    Update f2m.py
    Useless option.

--- f2m.py
+++ f2m.py
@@ -3,22 +3,15 @@ from mutagen.flac import FLAC
 from mutagen.easyid3 import EasyID3
 import mutagen.id3
 
-
 import os
 import sys
-
-if len(sys.argv) > 1:
-	substringTitle = int(sys.argv[1])
-	print("Remove first " + str(substringTitle) + " caracter.")
-else:
-	substringTitle = 0
 
 for subdir, dirs, files in os.walk('./'):
 	for file in files:
 		plop = file.split(".")
 		if plop[1] == "flac":
 			print ("file found : " + plop[0])
-			os.system("vlc -I dummy \"" + plop[0] + ".flac\" \":sout=#transcode{acodec=mpga,ab=192}:std{dst='" + plop[0][substringTitle:] + ".mp3',access=file}\" vlc://quit")
+			os.system("vlc -I dummy \"" + plop[0] + ".flac\" \":sout=#transcode{acodec=mpga,ab=192}:std{dst='" + plop[0] + ".mp3',access=file}\" vlc://quit")
 			# MP3
 			mf = MP3(plop[0] + ".mp3", ID3=EasyID3)
 			#FLAC
