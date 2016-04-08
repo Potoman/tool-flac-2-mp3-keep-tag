864b68c7586c454ea28b091fc5443abfea6142d1
Potoman <lepotoman@msn.com>
2016-04-08 23:25:52 +0200

    Added files via upload

--- f2m.py
+++ f2m.py
@@ -3,25 +3,59 @@ from mutagen.flac import FLAC
 from mutagen.easyid3 import EasyID3
 import mutagen.id3
 
+
 import os
 import sys
+#import property_reader
+
+if len(sys.argv) > 1:
+	substringTitle = int(sys.argv[1])
+	print("Remove first " + str(substringTitle) + " caracter.")
+else:
+	substringTitle = 0
 
 for subdir, dirs, files in os.walk('./'):
 	for file in files:
 		plop = file.split(".")
 		if plop[1] == "flac":
 			print ("file found : " + plop[0])
-			os.system("vlc -I dummy \"" + plop[0] + ".flac\" \":sout=#transcode{acodec=mpga,ab=192}:std{dst='" + plop[0] + ".mp3',access=file}\" vlc://quit")
+			os.system("vlc -I dummy \"" + plop[0] + ".flac\" \":sout=#transcode{acodec=mpga,ab=192}:std{dst='" + plop[0][substringTitle:] + ".mp3',access=file}\" vlc://quit")
 			# MP3
-			mf = MP3(plop[0] + ".mp3", ID3=EasyID3)
+			mf = MP3(plop[0][substringTitle:] + ".mp3", ID3=EasyID3)
 			#FLAC
 			ff = FLAC(plop[0] + ".flac")
 			#Let's go !
-			mf['title'] = ff['title']
-			mf['artist'] = ff['artist']
-			mf['album'] = ff['album']
-			mf['date'] = ff['date']
-			mf['genre'] = ff['genre']
-			mf['tracknumber'] = ff['tracknumber']
+			try:
+				mf['title'] = ff['title']
+			except:
+				print ("No title")
+			try:
+				mf['artist'] = ff['artist']
+			except:
+				print ("No artist")
+			try:
+				mf['album'] = ff['album']
+			except:
+				print ("No album")
+			try:
+				mf['date'] = ff['date']
+			except:
+				print ("No date")
+			try:
+				mf['genre'] = ff['genre']
+			except:
+				print ("No genre")
+			try:
+				mf['tracknumber'] = ff['tracknumber']
+			except:
+				print ("No genre")
 			mf.save()
-			
+
+#propgenerator = property_reader.property_sets("C:\\Users\\Maxime\\Music\\ESPERANZA SPALDING Chamber Music Society FLAC\\01 - Little Fly.flac")
+#for name, properties in propgenerator:
+#	print(name)
+#	for k, v in properties.items ():
+#		print("  ", k, "=>", v)
+
+
+#vlc -I dummy "03 - Really Very Small.flac" ":sout=#transcode{acodec=mpga,ab=192}:std{dst=output.mp3,access=file}" vlc://quit
